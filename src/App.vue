9cb77c34ae752467c902f79bd388f776c3716fc1
Vadim Shashin <enapiuz@gmail.com>
2017-05-15 23:24:33 +0300

    auto mounting

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="telemotor-application">
     {{ msg }}
   </div>
 </template>
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
+#telemotor-application {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// TODO: add body 50 padding, create bar div, set bar div -50 padding
+let bodyStyle = document.body.currentStyle || window.getComputedStyle(document.body);
+document.body.style.marginTop = parseInt(bodyStyle.marginTop) + 50 + "px";
+
+let container = document.createElement('div');
+container.id = "telemotor-container";
+container.style = "margin-top: -50px";
+document.body.appendChild(container);
 
 new Vue({
   el: '#telemotor-container',
